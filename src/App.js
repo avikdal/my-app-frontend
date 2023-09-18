f20444cd9153bb97b73b506329fa0167dbeb01dd
amanda vikdal <vikdal.amanda@gmail.com>
2023-09-18 10:31:44 -0400

    associated modal with create task button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import ToDosContainer from './components/ToDosContainer';
+import CreateTask from './modals/CreateTask';
 
 function App() {
+
+
   return (
-    <div className="App">
+    <>
       <div className='header text-center'>
         <Header />
       </div>
       <div className= "task-container">
         <ToDosContainer />
       </div>
-    </div>
+    </>
   );
 }
 
