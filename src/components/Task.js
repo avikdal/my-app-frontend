ff05df9b51102e1509b7d6b32287744340964604
amanda vikdal <vikdal.amanda@gmail.com>
2023-09-19 12:48:05 -0400

    updated Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Task = ({ taskInfo }) => {
 
-    // console.log("task info", taskInfo)
+    console.log("task info", taskInfo)
     
   return (
     <div>
-      <span>{taskInfo.task}</span>
+      {/* <span>{taskInfo.task}</span> */}
+      <FormGroup
+        check
+        inline
+      >
+        <Input type="checkbox" />
+        <Label check>{taskInfo.task}</Label>
+        <Button outline size="sm"> 🗑️ </Button>
+      </FormGroup>
     </div>
   )
 }
