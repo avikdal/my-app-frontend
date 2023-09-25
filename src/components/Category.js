b09d0753af2aa50c5812cf98c4ef3bed4a209d6e
amanda vikdal <vikdal.amanda@gmail.com>
2023-09-25 19:51:56 -0400

    cleaned up category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Collapse, CardBody, Card, CardHeader, Button, Badge, ListGroup, ListGroupItem, ListGroupItemHeading } from 'reactstrap';
+import { Collapse, Button, Badge, ListGroup, ListGroupItem, ListGroupItemHeading } from 'reactstrap';
 import Task from './Task'
 
 const Category = ({ category, name, tasks, deleteTask, deleteCategory, update }) => {
@@ -8,7 +8,6 @@ const Category = ({ category, name, tasks, deleteTask, deleteCategory, update })
 
   const toggle = () => setIsOpen(!isOpen);
 
-    // console.log("this is tasks in Category component", tasks)
 
   function handleClick(e){
     deleteCategory(category)
@@ -20,20 +19,12 @@ const Category = ({ category, name, tasks, deleteTask, deleteCategory, update })
   return (
     <div>
       <ListGroupItemHeading>
-      {/* <h3 onClick={toggle}> {name} </h3>  */}
         {name}{' '}
         <Badge color="primary" onClick={toggle} >
           {isOpen ? "Hide Tasks" : "Show Tasks" }
         </Badge>
       </ListGroupItemHeading>
-      {/* <h3 onClick={toggle} >{name}</h3> */}
       <Collapse isOpen={isOpen}>
-        {/* <Card>
-          <CardBody>
-            {tasks.map((task) => <Task key={task.id} taskInfo={task} deleteTask={deleteTask} />)}
-          </CardBody>
-          <Button color="secondary" onClick={handleClick}> Delete Entire Category </Button>
-        </Card> */}
         <ListGroup>
           <ListGroupItem>
           {tasks.map((task) => <Task key={task.id} taskInfo={task} update={update} deleteTask={deleteTask} />)}
