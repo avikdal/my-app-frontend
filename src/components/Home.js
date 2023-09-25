459651578c23a99af82d66badf31cacdc3cb6e0c
amanda vikdal <vikdal.amanda@gmail.com>
2023-09-25 15:01:27 -0400

    made home page less sad

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1> Hello Home Page </h1>
+      <h1 style={{ padding: '20px', textAlign: 'center' }}> Hello Home Page </h1>
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+      <p>Here's your tip of the day:</p>
+      <p>"Don't multitask. Focus on one task at a time for better results."</p>
+      </div>
     </div>
   )
 }
